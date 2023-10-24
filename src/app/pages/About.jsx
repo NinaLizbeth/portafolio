092069717cd9cb51bfed0f4cd6c1583246b26070
Carolina Hernandez <hernandez.carolina@live.cl>
2023-10-23 22:55:26 -0300

    [feat]: about, skill, estilos

---
+++ src/app/pages/About.jsx
@@ -0,0 +1,17 @@
+
+ const About = () => {
+  return (
+    <>
+<section>
+<article className=" w-full h-full bg-violet-400 flex flex-row gap-20 p-24 justify-around">
+   
+   <div className="w-96 h-96 border-4 rounded-xl "> <hr className="my-10 border-t-2 border-withe- border-2 w-full mx-auto" /></div>
+  <div className="w-96 h-96 border-4 rounded-xl"><hr  className="my-10 border-t-2 border-withe- border-2 w-full mx-auto" /></div>
+  <div className="w-96 h-96 border-4 rounded-xl"><hr  className="my-10 border-t-2 border-withe- border-2 w-full mx-auto" /></div>
+ 
+</article>
+</section>
+    </>
+  )
+}
+export default About
---
+++ src/app/pages/Skill.jsx
@@ -0,0 +1,17 @@
+
+
+ const Skill = () => {
+  return (
+
+    <>
+
+<section className="bg-gray-800 h-48">
+<h3 className="text-white text-center text-5xl p-10">Mis habilidades...</h3>
+
+    </section>
+    
+    
+    </>
+  )
+}
+export default Skill
