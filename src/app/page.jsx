b5b947057c086113bbc75a0a6f3c7b7990353886
Carolina Hernandez <hernandez.carolina@live.cl>
2023-11-13 12:34:34 -0300

    hamburguer y estilos

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,7 +10,7 @@ import Image from 'next/image'
 
 
 
-export default function ome() {
+export default function Home() {
 
   return (
     <>
@@ -21,7 +21,7 @@ export default function ome() {
         <div className='h-screen bg-green-300 flex justify-center items-center w-full lg:w-3/5  flex-col   p-16 lg:pr-52 '>
 
         
-<article className='text-3xl '>
+<article className='text-3xl lg:text-6xl '>
           <p style={fontSatisfy.style} className=' text-violet-400 lg:animate-bounce lg:text-start w-full   mb-3 lg:ml-0   mt-20 md:mt-40' >👋 Hola, soy Nina </p>
         
           <p className=' text-center lg:text-left   lg:text-7xl font-bold mb-7 text-slate-700  '>
