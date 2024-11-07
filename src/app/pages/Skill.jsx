29a2ebf4357d3bfc03bc5cecde667f9aca500763
Carolina Hernandez <hernandez.carolina@live.cl>
2024-11-07 12:31:11 -0300

    add

--- src/app/pages/Skill.jsx
+++ src/app/pages/Skill.jsx
@@ -4,14 +4,14 @@ import { motion } from "framer-motion"
 
 
 const Skill = () => {
-  const  variantes ={
-    entrada:{
+  const variantes = {
+    entrada: {
       y: 0,
-      opacity:1
+      opacity: 1
     },
-    inicial:{
+    inicial: {
       y: -100,
-      opacity:0
+      opacity: 0
     }
   }
 
@@ -22,76 +22,78 @@ const Skill = () => {
     <>
 
       <section
-      
 
-      id="sectionId" className="bg-pink-300 h-auto" >
-       
+
+        id="sectionId" className="bg-pink-300 h-auto" >
+
         <h3 style={fontRampart.style} className="text-white text-center lg:text-8xl text-5xl p-10">My Skills</h3>
         <motion.div
-        
-        transition={{duration:1}}
-        initial="inicial"
-        whileInView="entrada"
-        variants={variantes}
-                >
-        <div className="flex flex-col lg:flex-row lg:justify-around justify-center ">
-          <div>
-            <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-40 text-center">Frontend</h4>
-            <ul className="lg:p-0 p-14  lg:mr-36 lg:mt-6">
-              <li style={fontRampart.style} className="bg-yellow-200 text-black text-center rounded-3xl text-3xl mb-4 shadow-xl ">Html</li>
 
-              <li style={fontRampart.style} className=" bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl" >Css</li>
+          transition={{ duration: 1 }}
+          initial="inicial"
+          whileInView="entrada"
+          variants={variantes}
+        >
+          <div className="flex flex-col lg:flex-row lg:justify-around justify-center ">
+            <div>
+              <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-40 text-center">Frontend</h4>
+              <ul className="lg:p-0 p-14  lg:mr-36 lg:mt-6">
+                <li style={fontRampart.style} className="bg-yellow-200 text-black text-center rounded-3xl text-3xl mb-4 shadow-xl ">Html</li>
 
-              <li style={fontRampart.style} className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">JavaScript</li>
-              <li style={fontRampart.style} className="bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">React</li>
-              <li style={fontRampart.style} className="bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4  shadow-xl">NextJS</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">Boostrap</li>
-              <li style={fontRampart.style}className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">Tailwind</li>
-            </ul>
-            
-            
-            <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-36 text-center mt-20">Tools</h4>
-            <ul className="lg:p-0 p-14 lg:mr-0 lg:mt-6 lg:w-[70%]">
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">Git</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >GitHub</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Postman</li>
-            
-         
+                <li style={fontRampart.style} className=" bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl" >Css</li>
+
+                <li style={fontRampart.style} className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">JavaScript</li>
+                <li style={fontRampart.style} className="bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">React</li>
+                <li style={fontRampart.style} className="bg-yellow-200  text-black text-center  rounded-3xl text-3xl mb-4  shadow-xl">NextJS</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">Boostrap</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black text-center  rounded-3xl text-3xl mb-4 shadow-xl">Tailwind</li>
               </ul>
-              
-              </div>
-          
-          <div>
-            <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-40 text-center">Backend</h4>
-            <ul className="lg:p-0 p-14 lg:mr-36 lg:mt-6">
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">NodeJS</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">Express</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >MongoDB</li>
 
-              
-              <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-30 text-center mt-20">Cloud/PAAS</h4>
-            <ul className="lg:p-0 p-14 lg:mr-0 lg:mt-6">
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Firebase</li>
-              <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Vercel</li>
-         <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Render</li>
-         
+
+              <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-36 text-center mt-20">Tools</h4>
+              <ul className="lg:p-0 p-14 lg:mr-0 lg:mt-6 lg:w-[70%]">
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">Git</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >GitHub</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Postman</li>
+
+
               </ul>
-              
+
+            </div>
+
+            <div>
+              <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-40 text-center">Backend</h4>
+              <ul className="lg:p-0 p-14 lg:mr-36 lg:mt-6">
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">NodeJS</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl ">Express</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >MongoDB</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Python/Django</li>
+                <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >SQL /PostgreSQL</li>
 
 
-      
+                <h4 style={fontRampart.style} className="lg:text-5xl text-4xl text-white lg:mr-30 text-center mt-20">Cloud/PAAS</h4>
+                <ul className="lg:p-0 p-14 lg:mr-0 lg:mt-6">
+                  <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Firebase</li>
+                  <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Vercel</li>
+                  <li style={fontRampart.style} className="bg-yellow-200 text-black rounded-3xl text-center text-3xl mb-4 shadow-xl" >Render</li>
 
-              
-            </ul>
+                </ul>
+
+
+
+
+
+
+              </ul>
+            </div>
           </div>
-        </div>
-        <Image
-        width={500}
-        height={500}
-        
-        className=" hidden md:block   ml-[35%] -mt-72  " src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1698462236/Dise%C3%B1o_sin_t%C3%ADtulo_47_z9qngs.png" alt="" />
+          <Image
+            width={500}
+            height={500}
 
-</motion.div>
+            className=" hidden md:block   ml-[35%] -mt-72  " src="https://res.cloudinary.com/dt7h6qci4/image/upload/v1698462236/Dise%C3%B1o_sin_t%C3%ADtulo_47_z9qngs.png" alt="" />
+
+        </motion.div>
       </section>
 
 
